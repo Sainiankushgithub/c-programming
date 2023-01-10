609858cb4fbe09e2bdd5a07aa37163f1ef35d24c
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-01-10 23:30:27 +0530

    programms

---
+++ Functions/Function4.cpp
@@ -0,0 +1,32 @@
+// ** 2D ARRAY USING FUNCTION
+// #include<bits/stdc++.h>
+// using namespace std;
+// void printarray(int arr[4][4]);
+// int main()
+// {
+//     cout<<"Here 2D array is 4*4 \n";
+//     int arr[4][4];
+//     cout<<"Enter the 2D array elements\n";
+//     for(int i=0;i<4;i++)
+//     {
+//         for(int j=0;j<4;j++)
+//         {
+//             cin>>arr[i][j];
+//         }
+//         cout<<"Enter the number of elements for next row\n";
+//     }
+//        printarray(arr);
+//    return 0;
+// }
+//  void printarray(int arr[4][4])
+//     {
+//         int arr1[4][4];
+//         for(int i=0;i<4;i++)
+//         {
+//             for(int j=4;i>=0;j--)
+//             {
+//                 arr1[i][j]=arr[i][j];
+//             }
+//         }
+//     }
+        
