6a46fac8f171e5efcbf61d3f990a931c7ab4ba17
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-05-31 23:10:22 +0530

    pragramm

--- C++ STL/vector/vector2.cpp
+++ C++ STL/vector/vector2.cpp
@@ -26,10 +26,13 @@ int main()
     display(v);
 
     // INSERT FUNCTION 
+    // INSERT AN ELEMETN AT THE GIVEN INDEX
     cout<<"Dipslaying your value after insertion\n";
     v.insert(v.begin()+2,100);
     display(v);
+    // INSERT THE MULTIPLE ELEMENT AT THE GIVEN INDEX
     v.insert(v.end()-1,2,50);
     display(v);
+    // 
 return 0;
 }
