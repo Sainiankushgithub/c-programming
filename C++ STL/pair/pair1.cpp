57209f5cba9dfd7aadde30b90eb1f019ad0865b0
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-06-02 23:57:48 +0530

    programm

---
+++ C++ STL/pair/pair1.cpp
@@ -0,0 +1,14 @@
+// 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector< pair<int,string>>vp={{1,"APPLE"},{2,"ORANGE"}};
+    cout<<"Displaying your vector pair\n";
+    for(auto x:vp)
+    {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+return 0;
+}
